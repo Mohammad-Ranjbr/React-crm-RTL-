9b99f33f3e3d9de330ab8af49dc66b352dd93837
shahab-i317 <shahab-i317@sdit.in>
2024-03-03 14:15:53 +0330

    change subject to persian

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -16,10 +16,10 @@ import statusCards from '../assets/JsonData/status-card-data.json'
 
 const chartOptions = {
     series: [{
-        name: 'Online Customers',
+        name: 'کاربران آنلاین',
         data: [40,70,20,90,36,80,30,91,60]
     }, {
-        name: 'Store Customers',
+        name: 'کاربران آفلاین',
         data: [40, 30, 70, 80, 40, 16, 40, 20, 51, 10]
     }],
     options: {
@@ -34,7 +34,7 @@ const chartOptions = {
             curve: 'smooth'
         },
         xaxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
+            categories: ['فروردین', 'خرداد', 'مرداد', 'شهریور', 'ابان', 'آذر', 'بهمن', 'اسفند']
         },
         legend: {
             position: 'top'
@@ -47,33 +47,33 @@ const chartOptions = {
 
 const topCustomers = {
     head: [
-        'user',
-        'total orders',
-        'total spending'
+        'کاربر',
+        'مجموعه فعالیت ها',
+        ' مجموعه زمان'
     ],
     body: [
         {
-            "username": "john doe",
+            "username": "عباس  ",
             "order": "490",
             "price": "$15,870"
         },
         {
-            "username": "frank iva",
+            "username": "یاسر",
             "order": "250",
             "price": "$12,251"
         },
         {
-            "username": "anthony baker",
+            "username": "یوسف",
             "order": "120",
             "price": "$10,840"
         },
         {
-            "username": "frank iva",
+            "username": "یونس",
             "order": "110",
             "price": "$9,251"
         },
         {
-            "username": "anthony baker",
+            "username": "جابر",
             "order": "80",
             "price": "$8,840"
         }
@@ -168,7 +168,7 @@ const Dashboard = () => {
 
     return (
         <div>
-            <h2 className="page-header">Dashboard</h2>
+            <h2 className="page-header">داشبورد</h2>
             <div className="row">
                 <div className="col-6">
                     <div className="row">
@@ -205,7 +205,7 @@ const Dashboard = () => {
                 <div className="col-4">
                     <div className="card">
                         <div className="card__header">
-                            <h3>top customers</h3>
+                            <h3>لیست </h3>
                         </div>
                         <div className="card__body">
                             <Table
@@ -216,14 +216,14 @@ const Dashboard = () => {
                             />
                         </div>
                         <div className="card__footer">
-                            <Link to='/'>view all</Link>
+                            <Link to='/'>مشاهده همه</Link>
                         </div>
                     </div>
                 </div>
                 <div className="col-8">
                     <div className="card">
                         <div className="card__header">
-                            <h3>latest orders</h3>
+                            <h3>آخرین فعالیت ها</h3>
                         </div>
                         <div className="card__body">
                             <Table
@@ -234,7 +234,7 @@ const Dashboard = () => {
                             />
                         </div>
                         <div className="card__footer">
-                            <Link to='/'>view all</Link>
+                            <Link to='/'>مشاهده همه</Link>
                         </div>
                     </div>
                 </div>
